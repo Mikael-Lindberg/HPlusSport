1a3c99da9258442e0f8a70ced253dcbb3499437a
mikael <mikael.lindberg93@gmail.com>
2024-03-05 07:11:53 +0100

    implemented sorting

--- HPlusSport.API/Models/QueryParameters.cs
+++ HPlusSport.API/Models/QueryParameters.cs
@@ -12,5 +12,23 @@
                 _size = Math.Min(_maxSize, value);
             }
         }
+
+        public string SortBy { get; set; } = "Id";
+
+        private string _sortOrder = "asc";
+        public string SortOrder
+        {
+            get
+            {
+                return _sortOrder;
+            }
+            set
+            {
+                if (value == "asc" || value == "desc")
+                {
+                    _sortOrder = value;
+                }
+            }
+        }
     }
 }
