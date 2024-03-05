3ffde0bf1774e86e9ecf8af818333774b0712777
mikael <mikael.lindberg93@gmail.com>
2024-03-05 07:23:30 +0100

    created more advanced search

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -32,6 +32,13 @@ namespace HPlusSport.API.Controllers
                 products = products.Where(p => p.Price <= queryParameters.MaxPrice.Value);
             }
 
+            if (!string.IsNullOrEmpty(queryParameters.SearchTerm))
+            {
+                products = products.Where(
+                    p => p.Sku.ToLower().Contains(queryParameters.SearchTerm.ToLower()) ||
+                    p.Name.ToLower().Contains(queryParameters.SearchTerm.ToLower()));
+            }
+
             if (!string.IsNullOrEmpty(queryParameters.Sku))
             {
                 products = products.Where(
